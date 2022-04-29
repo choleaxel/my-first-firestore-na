67a7fe9af6d7f44fc96e64fb08ed701f8f3f6e5f
Nichole Axel <nfrancis22@gmail.com>
2022-04-29 11:16:39 -0400

    created first restaurant

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("path/to/serviceAccountKey.json");
+const serviceAccount = require('./credentials.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
+const db = admin.firestore();
+
+
+const restaurant = {
+  name: 'Mister 01',
+  address: '555 N Federal HWY',
+  cuisine: 'Pizza',
+  rating: 4.9,
+  phone: '(786) 677-2903',
+}
+
+db.collection('restaurants').add(restaurant)//referencing db, creating a collection named 'restaurants' to it
+//also adding the data from the above added info
+  .then(doc => console.log('Created restaurant', doc.id)) //Will print and also include the id created by Firebase
+  .catch(err => console.error(err)) //handling an error
+
+
+
+
